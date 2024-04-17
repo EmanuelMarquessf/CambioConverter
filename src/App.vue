a52ff21ce7ff57f12770aad1637e4cac0d5e553a
EmanuelMarquessf <emanuel.marquessf@gmail.com>
2024-04-17 18:18:37 -0300

    Layout Design

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,141 @@
 <script setup>
+  import { ref } from 'vue'
   import { fetchData } from './services/freecurrency.service.js'
+  const data = async () => await fetchData();
 
-  const cambio = fetchData()
 
-  console.log(cambio)
+  console.log(data)
+
+  const valueToConvert = ref(0);
 </script>
 
 <template>
-  <div class="hello">
-    <span class="child-hello">b</span>
+  <div class="container">
+    <!-- <span v-for="cambio in cambios" :key="cambio" class="child-hello">{{cambio}}</span> -->
+    <form class="formContainer" action="">
+      <div class="selectsContainer">
+        <div class="selectContainer">
+          <label for="cars">Convert to</label>
+          <select name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+        <button><></button>
+        <div class="selectContainer">
+          <label for="cars">From:</label>
+          <select name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+      </div>
+      <div class="flexContainerColumn">
+        <label for="valueToConvert">Value</label>
+        <div class="inputContainer">
+          <span>R$</span>
+          <input name="valueToConvert" type="number" >
+        </div>
+      </div>
+    </form>
+    <label for="valueToConvert">Converter para</label>
+    <div class="inputContainer">
+      <span>R$</span>
+      <input name="valueToConvert" type="number" >
+    </div> 
   </div>
 </template>
 
 <style lang="scss">
-  $bgColor: red;
+  $bgColor: #242424;
+  $inputColor: #131315 ;
+  $text: #e1e1e2;
+  $secondaryText: #787883;
 
-  .hello{
+  .flexContainerRow{
+    display: flex;
+    flex-direction: row;
+    gap:0.5rem;
+  }
+
+  .flexContainerColumn{
+    display: flex;
+    flex-direction: column;
+    gap:0.5rem;
+  }
+  .container{
     background-color: $bgColor;
-    .child-hello{
-      background-color: blue;
+    padding: 2rem;
+    width: 40rem;
+    height: 40rem;
+    border-radius: 1rem;
+    .formContainer{
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+    label{
+      color: $text;
+      font-size: larger;
+    }
+
+    .selectsContainer{
+      display: flex;
+      flex-direction: row;
+      align-items:flex-end;
+      gap:2rem;
+      button{
+        border: none;
+        height: 40px;
+        background-color: $inputColor;
+        color: $secondaryText;
+      }
+      .selectContainer{
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        gap: 0.5rem;
+        select{
+          border: none;
+          border-radius:0.5rem;
+          font-size: x-large;
+          padding: 8px 4px;
+          background-color: $inputColor;
+          color: $secondaryText;
+        }
+      }
+    }
+    .inputContainer{
+      align-items: center;
+      background-color: $inputColor;
+      border-radius:0.5rem;
+      color: $text;
+      display: flex;
+      flex-direction: row;
+      
+      padding: 0.7rem;
+      span{
+        font-size:x-large;
+      }
+      input[type="number"] {
+        &::-webkit-inner-spin-button,
+        &::-webkit-outer-spin-button {
+          -webkit-appearance: none;
+          appearance: none;
+          margin: 0;
+        }
+        &:focus {
+          outline: none;
+        }
+        background-color:  $inputColor;
+        border: none;
+        color: $secondaryText;
+        font-size: x-large;
+      }
     }
   }
 </style>
--- src/services/freecurrency.service.js
+++ src/services/freecurrency.service.js
@@ -1,12 +1,10 @@
 import { apiKey } from "../config/config";
 export async function fetchData(){
   const currencies = 'BRL%2CEUR';
-  console.log({apiKey})
-  const data = 0
-  fetch(
+
+  const data = fetch(
     `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&currencies=BRL%2CEUR`
   ).then(response => response.json())
-  .then(json=> console.log({json}))
   return data;
 }
 
